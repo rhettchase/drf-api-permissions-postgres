b804c36a56d9bcbfcc8c435587e7e6debb5843e9
RHETT CHASE <rhettjchase@gmail.com>
2024-02-20 14:41:33 -0800

    updates permissions

--- brews/permissions.py
+++ brews/permissions.py
@@ -2,15 +2,21 @@ from rest_framework import permissions
 
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Object-level permission to only allow owners of an object to edit it.
+    Assumes the model instance has an `owner` attribute.
+    """
     def has_object_permission(self, request, view, obj):
-
-        # hover over SAFE_METHODS to see which qualify
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        # safe for reading
         if request.method in permissions.SAFE_METHODS:
             return True
 
-        # if we're allowing the purchaser to be null in Model
+        # if we're allowing the owner to be null in Model
         # then this will check for that case and allow access
         if obj.owner is None:
             return True
 
+        # can only make changes to stuff if you are the owner
         return obj.owner == request.user
